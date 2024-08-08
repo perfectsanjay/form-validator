d588e7f5ca56bec089e28b80a4559a8069baf012
Sanjay Singh <sanjay007abd@gmail.com>
2024-08-08 19:31:17 +0530

    validate form through html

---
+++ script.js
@@ -0,0 +1,52 @@
+const form = document.getElementById('form')
+const password1El = document.getElementById('password1')
+const password2El = document.getElementById('password2')
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message')
+
+let isValid = false;
+let passwordMatch = false;
+
+// validate form
+function ValidateForm(){
+    isValid = form.checkValidity();
+    if (!isValid) {
+        message.textContent = 'Please fill out all fields'
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        return;
+    }
+    if (password1El.value === password2El.value){
+        passwordMatch = true
+        password1El.style.color = 'green'
+        password2El.style.color = 'green'
+    }
+    else{
+        passwordMatch = false
+        message.textContent = 'Make sure your password match'
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        password1El.style.color = 'red'
+        password2El.style.color = 'red'
+        return;
+
+    }
+    // if form is valid and password is match
+    if (isValid && passwordMatch) {
+        message.textContent = 'Successfully Register'
+        message.style.color = 'green'
+        messageContainer.style.borderColor = 'green'
+
+    }
+    
+}
+
+// function for processing form data
+function processFormData(e){
+    e.preventDefault()
+    ValidateForm()
+}
+
+
+// add Event Listener
+form.addEventListener('submit',processFormData)
